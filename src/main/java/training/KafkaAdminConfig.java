822c1918a0b1cac0df1427e150676227c4645c72
Alex Dorogensky <alex@exebar.com>
2020-08-16 16:32:00 -0500

    move kafka admin, producer and consumer configuration into application.yml

--- src/main/java/training/KafkaAdminConfig.java
+++ src/main/java/training/KafkaAdminConfig.java
@@ -1,33 +1,10 @@
 package training;
 
-import org.apache.kafka.clients.admin.AdminClientConfig;
 import org.apache.kafka.clients.admin.NewTopic;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.config.TopicBuilder;
-import org.springframework.kafka.core.KafkaAdmin;
 
-import java.util.HashMap;
-import java.util.Map;
-
-@Configuration
 public class KafkaAdminConfig {
-
-    @Value("${spring.kafka.bootstrap-servers}")
-    private String bootstrapServers;
-
-    @Bean
-    Map<String, Object> kafkaAdminProps() {
-        Map<String, Object> config = new HashMap<>();
-        config.put(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
-        return config;
-    }
-
-    @Bean
-    KafkaAdmin kafkaAdmin() {
-        return new KafkaAdmin(kafkaAdminProps());
-    }
 
     @Bean
     public NewTopic topic() {
--- src/main/java/training/KafkaTrainingApplication.java
+++ src/main/java/training/KafkaTrainingApplication.java
@@ -2,13 +2,16 @@ package training;
 
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.core.env.Environment;
+import org.springframework.kafka.annotation.EnableKafka;
 
 import java.time.LocalDateTime;
 
 @SpringBootApplication
+@EnableKafka
 public class KafkaTrainingApplication implements CommandLineRunner {
 
 	private final ApplicationContext applicationContext;
